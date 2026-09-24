a86e3666141f93aca234a5fefcc35141802e7967
Hana Birhanu <hanabirhanuaragaw@gmail.com>
2026-09-24 09:37:39 +0000

    built a simple profile app

--- build-a-random-joke-app/server.js
+++ build-a-random-joke-app/server.js
@@ -1,19 +1,23 @@
 const express=require('express');
 const app=express();
 app.use(express.json());
+const port=3000;
 const jokes=[
-    'How do java developers get rich? They inherit',
-    'Why did the sql sit alone? Because he can not join tables',
-    'I told my computer I needed a break, and it said "No problem, I’ll go to sleep."',
-  'Why do Java developers wear glasses? Because they don’t see sharp.'
-]
-app.get('/about',(req,res)=>{
-    res.json("This joke was built in express")
+     "Why do programmers prefer dark mode? Because light attracts bugs!",
+  "There are only 10 kinds of people in the world: those who understand binary and those who don't.",
+  "I told my computer I needed a break, and it said \"No problem, I'll go to sleep.",
+  "Why do Java developers wear glasses? Because they don't see sharp.",
+];
+app.get('/', (req, res) => {
+  res.status(200).send(`Welcome to the Random Joke Server! Visit /joke to get a random joke.`);
 });
-app.get('/jokes',(req,res)=>{
-     const rand=Math.floor(Math.random()*jokes.length);
-     res.send(jokes[rand]);
+app.get('/about',(req,res)=>{
+     res.send("This Random Joke Server was built with Express.js");
+});
+app.get('/joke',(req,res)=>{
+     const randomJoke=jokes[Math.floor(Math.random()*jokes.length)];
+     res.status(200).send(randomJoke);
 })
 app.listen(3000,()=>{
-    console.log('Server started listening to http://localhost:3000');
+    console.log(`Server started listening to http://localhost:${port}`);
 })
